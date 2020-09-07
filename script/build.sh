b7a5fab738b58ef7b27f37eb3dff3edd685565a8
dfeliz <nonifeliz1@gmail.com>
2020-09-07 19:52:38 -0400

    first commit

---
+++ script/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+build() {
+    echo 'building react'
+
+    rm -rf dist/*
+
+    export INLINE_RUNTIME_CHUNK=false
+    export GENERATE_SOURCEMAP=false
+
+    react-scripts build
+
+    mkdir -p dist
+    cp -r build/* dist
+
+    rm -rf build/*
+    rm build
+
+    mv dist/index.html dist/popup.html
+}
+
+build
