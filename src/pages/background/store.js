364d31a51bc6785565431eb433c7437a37dea229
dfeliz <nonifeliz1@gmail.com>
2020-09-13 20:25:45 -0400

    ADD: batel-preset-env, redux-persist, redux-persist-chrome-storage

--- src/pages/background/store.js
+++ src/pages/background/store.js
@@ -1,10 +1,22 @@
 import {createStore} from 'redux';
 import {createBackgroundStore} from 'redux-webext';
+import { persistStore, persistReducer } from 'redux-persist'
+import createChromeStorage from 'redux-persist-chrome-storage'
 import {INCREMENT_UI_COUNTER, DECREMENT_UI_COUNTER, INCREMENT_CONTENTSCRIPTS_COUNTER} from '../constants';
 import reducer from './reducers';
 import {incrementUICounter, decrementUICounter, incrementContentScriptsCounter} from './actions';
 
-const store = createStore(reducer);
+const storage = createChromeStorage(window.chrome, 'sync');
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+  
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+const store = createStore(persistedReducer);
+// const persistor = persistStore(store)
 
 export default createBackgroundStore({
     store,
